b136bd1879fa57b6255819067aeee48cc53bf5a1
Chad <chad.n.palmer@gmail.com>
2018-07-03 13:56:12 -0500

    Insert method added

--- src/ds/binarysearchtree/App.java
+++ src/ds/binarysearchtree/App.java
@@ -4,5 +4,9 @@ public class App {
 
     public static void main(String[] args) {
 
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(10, "Ten");
+        tree.insert(20, "Twenty");
+        tree.insert(15, "Fifteen");
     }
 }
--- src/ds/binarysearchtree/BinarySearchTree.java
+++ src/ds/binarysearchtree/BinarySearchTree.java
@@ -1,4 +1,37 @@
 package ds.binarysearchtree;
 
 public class BinarySearchTree {
+
+    private Node root;
+
+    public void insert(int key, String value) {
+
+        Node newNode = new Node(key, value);
+
+        if (root == null) {
+            root = newNode;
+        }
+        else {
+            Node current = root;        // A node for traversing tree
+            Node parent;
+
+            while (true) {
+                parent = current;
+                if (key < current.key) {
+                    current = current.leftChild;
+                    if (current == null) {  // It's parent is the leaf node
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                }
+                else {
+                    current = current.rightChild;
+                    if (current == null) {
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
