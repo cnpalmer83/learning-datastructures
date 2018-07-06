aa801fbb915178ab40db99127d070e3e2381c0e5
Chad <chad.n.palmer@gmail.com>
2018-07-06 12:51:10 -0500

    progression on BST delete method

--- src/ds/binarysearchtree/BinarySearchTree.java
+++ src/ds/binarysearchtree/BinarySearchTree.java
@@ -59,13 +59,79 @@ public class BinarySearchTree {
 
     public boolean remove(int key) {
 
+        // Placeholders to traverse tree
+        Node currentNode = root;
+        Node parentNode = root;     // parent to whatever currentNode is
+        boolean isLeftChild = false;    // Determines if deleted node is left or right child
+
+        // First, we have to find the node that is to be deleted:
+        while (currentNode.key != key) {
+                parentNode = currentNode;
+            if (key > currentNode.key) {
+                currentNode = currentNode.rightChild;
+                isLeftChild = false;
+            }
+            else {
+                currentNode = currentNode.leftChild;
+                isLeftChild = true;
+            }
+            if (currentNode == null)
+                return false;           // if key not found in tree
+        }
+
+        // Now the node to delete has been found.  We have to account for three
+        // possible scenarios for the node.  It might be a leaf, it might have
+        // one child, or it might have two children.  For readability, currentNode
+        // will be renamed to nodeToDelete (this is optional)
+        Node nodeToDelete = currentNode;
+
         // Scenario 1: Is the node a leaf?
+        // Assign parent node reference to null, Java takes care of the rest
+        if (nodeToDelete.leftChild == null && nodeToDelete.rightChild == null) {
+            if (nodeToDelete == root)
+                root = null;
+            else if (isLeftChild)
+                parentNode.leftChild = null;
+            else
+                parentNode.rightChild = null;
+        }
 
         // Scenario 2: Does the node have one child?
+        // Copy subtree into deleted node position
+
+        // Node to delete has one child to the left and rightChild is null
+        else if (nodeToDelete.rightChild == null) {
+            if (nodeToDelete = root)
+                root = nodeToDelete.leftChild;
+            else if (isLeftChild)
+                parentNode.leftChild = nodeToDelete.leftChild;
+            else
+                parentNode.rightChild = nodeToDelete.leftChild;
+        }
+
+        // Node to delete has one child to the right and leftChild is null
+        else if (nodeToDelete.leftChild == null) {
+            if (nodeToDelete = root)
+                root = nodeToDelete.rightChild;
+            else if (isLeftChild)
+                parentNode.leftChild = nodeToDelete.rightChild;
+            else
+                parentNode.rightChild = nodeToDelete.rightChild;
+        }
 
         // Scenario 3: Does the node have two children?
+        // Replace deleted node with smallest node from subtree (in order successor)
+        else {
+            // Step 1: find the successor to replace deleted node
+            Node successor = getSuccessor(nodeToDelete);
+
+        }
 
         return true;
+    }
+
+    private Node getSuccessor(Node nodeToDelete) {
+
     }
 
 
