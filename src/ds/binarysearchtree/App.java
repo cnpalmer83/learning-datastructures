0ccdf823fbcfb68677080ef99887703f8b6f5419
Chad <chad.n.palmer@gmail.com>
2018-07-09 14:01:31 -0500

    BST randomly populated, displayTree() added

--- src/ds/binarysearchtree/App.java
+++ src/ds/binarysearchtree/App.java
@@ -1,19 +1,28 @@
 package ds.binarysearchtree;
 
+import java.util.Random;
+
+/*
+    This program implements the given BST by creating a BST object and then
+    populating it with 8 random integers between 0-99.  Order of entry is displayed
+    prior to the tree to examine how each node is placed.
+ */
 public class App {
 
     public static void main(String[] args) {
 
         BinarySearchTree tree = new BinarySearchTree();
-        tree.insert(10, "Ten");
-        tree.insert(20, "Twenty");
-        tree.insert(15, "Fifteen");
+        Random rand = new Random();
 
-
+        System.out.print("Order of entry: " + "{ ");
+        for (int i = 0; i < 8; i++) {
+            int rand_int = rand.nextInt(100);
+            tree.insert(rand_int, "Random value");
+            System.out.print(rand_int + " ");
+        }
+        System.out.println("}");
         System.out.println(tree.findMaximum().key);
         System.out.println(tree.findMinimum().key);
-
-        System.out.println(tree.remove(10));
-        System.out.println(tree.findMinimum().key);
+        tree.displayTree();
     }
 }
--- src/ds/binarysearchtree/BinarySearchTree.java
+++ src/ds/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package ds.binarysearchtree;
 
+
+import java.util.Stack;
+
 public class BinarySearchTree {
 
     private Node root;
@@ -164,5 +167,47 @@ public class BinarySearchTree {
         return successor;
     }
 
+    public void displayTree() {
+        Stack globalStack = new Stack();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+        System.out.println("......................................................");
 
+        while(isRowEmpty==false) {
+            Stack localStack = new Stack();
+            isRowEmpty = true;
+            for(int j=0; j<nBlanks; j++) {
+                System.out.print(" ");
+            }
+
+            while(globalStack.isEmpty()==false) {
+                Node temp = (Node)globalStack.pop();
+                if(temp != null) {
+                    System.out.print(temp.key);
+                    localStack.push(temp.leftChild);
+                    localStack.push(temp.rightChild);
+                    if(temp.leftChild != null || temp.rightChild != null) {
+                        isRowEmpty = false;
+                    }
+                } else {
+                    System.out.print("><");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+
+                for(int j=0; j<nBlanks*2-2; j++) {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+            nBlanks = nBlanks/2;
+
+            while(localStack.isEmpty()==false)
+                globalStack.push( localStack.pop() );
+        }
+        System.out.println( "......................................................");
+
+    }
 }
